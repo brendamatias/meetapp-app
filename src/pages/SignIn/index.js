4ea2f9f06395515d72cf00d236ca9a8486388bee
Brenda Matias <brendamatias.sobral@gmail.com>
2019-10-17 21:47:11 -0300

    feat: Sign In page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,14 +1,43 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+
+import logo from '~/assets/M.png';
 
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>Hello</Text>
+      <Container>
+        <Image source={logo} alt="Meetapp" />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Your e-mail"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Your secret password"
+          />
+          <SubmitButton onPress={() => {}}>Login</SubmitButton>
+        </Form>
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Create my account</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
